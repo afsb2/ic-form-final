141be46ceaac33984e83713054152bab886b6e0b
Arthur Freitas Sultanum Burgos <afsb2@cesar.school>
2026-07-07 12:12:19 -0300

    Update Questão1.js

--- Questão1.js
+++ Questão1.js
@@ -4,18 +4,31 @@ function validarFormulario() {
     let email = document.getElementById("email").value;
     let senha = document.getElementById("senha").value;
 
-    if (nome == "") {
-        alert("O nome não pode estar vazio.");
+    // Validação do nome
+    if (nome.trim() == "") {
+        alert("Erro: o campo Nome não pode estar vazio.");
+        return false;
+    }
+
+    // Validação do email
+    if (email.trim() == "") {
+        alert("Erro: o campo Email não pode estar vazio.");
         return false;
     }
 
     if (!email.includes("@")) {
-        alert("O email deve conter @.");
+        alert("Erro: o email deve conter pelo menos um '@'.");
+        return false;
+    }
+
+    // Validação da senha
+    if (senha.trim() == "") {
+        alert("Erro: o campo Senha não pode estar vazio.");
         return false;
     }
 
     if (senha.length < 8) {
-        alert("A senha deve ter pelo menos 8 caracteres.");
+        alert("Erro: a senha deve possuir no mínimo 8 caracteres.");
         return false;
     }
 
